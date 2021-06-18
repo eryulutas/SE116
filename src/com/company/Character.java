ddccf05c34cf79db1617bc27d057c18316567e23
pelintuncc <pelinn.tunc10@hotmail.com>
2021-06-19 00:52:33 +0300

    Revert "a"
    This reverts commit b3b5f611effa840470bb2f220708ca320e26610e.

--- src/com/company/Character.java
+++ src/com/company/Character.java
@@ -6,7 +6,6 @@ public abstract class Character<T> {
 
 
 
-
     public Character(String name,int healthPoints,int maxHealthPoints,int attackDamage,int attackSpeed){
         this.name=name;
         this.healthPoints=healthPoints;
